3a7897163b81eb603117a62d885f5dec4b4df14f
Arnold ky <kysylvestre2@gmail.com>
2022-10-24 09:27:52 -0400

    initial commit

---
+++ 20221024-ArnoldKy-NYCSchools/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  20221024-ArnoldKy-NYCSchools
+//
+//  Created by Arnold Sylvestre on 10/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 20221024-ArnoldKy-NYCSchools/_0221024_ArnoldKy_NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0221024_ArnoldKy_NYCSchoolsApp.swift
+//  20221024-ArnoldKy-NYCSchools
+//
+//  Created by Arnold Sylvestre on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct _0221024_ArnoldKy_NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
